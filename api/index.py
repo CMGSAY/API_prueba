5815edaf5f3989acaacde8e9fe9427d4b4cc13e7
CMGSAY <carlos.garcia.say@gmail.com>
2026-04-18 00:29:54 -0600

    hoy si xd

--- api/index.py
+++ api/index.py
@@ -1,33 +1,24 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from datetime import datetime
 
 app = FastAPI()
-
-usuarios = {}
 
 class Login(BaseModel):
     correo: str
     contrasenia: str
 
-
-usuarios[1] = {
-    "nombre": "Carlos Garcia",
-    "correo": "carlosgarcias@umes.edu.gt",
-    "contrasenia": "1234"
+usuarios = {
+    "carlosgarcias@umes.edu.gt": "1234"
 }
-
 
 @app.get("/")
 def home():
-    return {"mensaje": "API funcionando"}
-
+    return {"mensaje": "API funcionando en Vercel"}
 
 @app.post("/login")
 def login(datos: Login):
 
-    for usuario in usuarios.values():
-        if usuario["correo"] == datos.correo and usuario["contrasenia"] == datos.contrasenia:
-            return {"mensaje": "Login exitoso"}
+    if datos.correo in usuarios and usuarios[datos.correo] == datos.contrasenia:
+        return {"mensaje": "Login exitoso"}
 
     raise HTTPException(status_code=401, detail="Credenciales incorrectas")
